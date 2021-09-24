2ad999bb6ed73b1040952f576bb2747e5834e645
Andrew Hany <andrewdodz51234@gmail.com>
2021-09-24 02:05:09 +0200

    Navbar adjustments

--- src/components/navBar/index.js
+++ src/components/navBar/index.js
@@ -1,39 +1,60 @@
 import React from "react";
 import useStyles from "./style";
-import { AppBar, Toolbar, Typography, IconButton, Tooltip } from "@material-ui/core";
+import { AppBar, Toolbar, Typography, IconButton, Tooltip, Badge, Grid } from "@material-ui/core";
 
-function NavBar() {
+function NavBar(props) {
     const styles = useStyles();
     return (
         <AppBar color="secondary" className={styles.appBar}>
             <Toolbar>
-                <Typography className={styles.text}>
-                    SWITCH ME
-                </Typography>
+                <Grid
+                    container 
+                    direction="row" 
+                    className={styles.gridTitle}>
+                    <Typography className={styles.text}>
+                        SWITCH ME
+                    </Typography>
+                </Grid>
+                
+                    <Grid 
+                        container 
+                        direction="row" 
+                        justifyContent="flex-end" 
+                        className={styles.gridIcons}>
+                        <Grid item xs={2} md={1}>
+                            <Tooltip title="Add Request" >
+                                <IconButton className={styles.icon} color="primary" href="addSwitch">
+                                    <ion-icon name="add" class={styles.ionIcon}></ion-icon>
+                                </IconButton>
+                            </Tooltip>
+                        </Grid>
 
-                <Tooltip title="Add Request">
-                    <IconButton className={styles.iconAdd} color="primary" href="addSwitch">
-                        <ion-icon name="add" class={styles.ionIcon}></ion-icon>
-                    </IconButton>
-                </Tooltip>
+                        <Grid item xs={2} md={1}>
+                            <Tooltip title="Filter">
+                                <IconButton className={styles.icon} color="primary" href="filter">
+                                    <ion-icon name="funnel" class={styles.ionIcon}></ion-icon>
+                                </IconButton>
+                            </Tooltip>
+                        </Grid>
 
-                <Tooltip title="Filter">
-                    <IconButton className={styles.iconFilter} color="primary" href="filter">
-                        <ion-icon name="funnel" class={styles.ionIcon}></ion-icon>
-                    </IconButton>
-                </Tooltip>
+                        <Grid item xs={2} md={1}>
+                            <Tooltip title="My Account">
+                                <IconButton className={styles.icon} color="primary" href="account">
+                                    <ion-icon name="person" class={styles.ionIcon}></ion-icon>
+                                </IconButton>
+                            </Tooltip>
+                        </Grid>
 
-                <Tooltip title="My Account">
-                    <IconButton className={styles.iconUser} color="primary" href="account">
-                        <ion-icon name="person" class={styles.ionIcon}></ion-icon>
-                    </IconButton>
-                </Tooltip>
-
-                <Tooltip title="Notifications">
-                    <IconButton className={styles.iconBell} color="primary" href="notifications">
-                        <ion-icon name="notifications" class={styles.ionIcon}></ion-icon>
-                    </IconButton>
-                </Tooltip>
+                        <Grid item xs={2} md={1}>
+                            <Tooltip title="Notifications">
+                                <IconButton className={styles.icon} color="primary" href="notifications">
+                                    <Badge badgeContent={parseInt(props.notifications) ? props.notifications : 0} color='primary' classes={{badge: styles.badge}}>
+                                        <ion-icon name="notifications" class={styles.ionIcon}></ion-icon>
+                                    </Badge>
+                                </IconButton>
+                            </Tooltip>
+                        </Grid>    
+                    </Grid>
             </Toolbar>
         </AppBar>
         )
