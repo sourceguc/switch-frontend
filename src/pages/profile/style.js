c7580e61c4a124964fb8a44c0f2b98843d56756b
Andrew Hany <68119650+Andrew51234@users.noreply.github.com>
2021-10-06 22:11:50 +0200

    Fixed cross-browser UI bug (#20)
    '-webkit-center' property was not recognized by Firefox so '-moz-center'
    was added as a fallback

--- src/pages/profile/style.js
+++ src/pages/profile/style.js
@@ -1,5 +1,6 @@
 import { makeStyles } from "@material-ui/core/styles";
-
+const isFirefox = typeof InstallTrigger !== 'undefined';
+const center = isFirefox ? '-moz-center' : '-webkit-center';
 export const useStyles = makeStyles((theme)=>({
     grid: {
         marginTop:'4em',
@@ -124,7 +125,7 @@ export const useStyles = makeStyles((theme)=>({
     },
     paper: {
         outline:'0',
-        textAlign: '-webkit-center'
+        textAlign: center
   },
 
 }));
